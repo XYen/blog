3625cd07e93142736deaf1975fffe9260766eddb
XYen <843121833@qq.com>
2018-07-02 20:25:12 +0800

    Finish user edit,update,index,and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,14 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+
+    }
+
     public function create()
     {
         return view('sessions.create');
@@ -24,13 +32,15 @@ class SessionsController extends Controller
 
             session()->flash('success','欢迎回来！');
 
-            return redirect()->route('users.show',[Auth::user()]);
+            return redirect()->intended(route('users.show',[Auth::user()]));
         }else{
 
            session()->flash('danger','很抱歉，你的邮箱和密码不匹配');
 
            return redirect()->back();
         }
+
+
     }
 
     public function destroy()
